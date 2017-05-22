fe286d0b7f2a686f29ee4201e61b00ee3bb7de47
Holly Murphy <holly.m.murphy@hotmail.com>
2017-05-22 13:25:24 -0500

    added silly_story builder and git loc to jsfundpartI

---
+++ js_fundamentals/JSFundPartI/scripts/be_the_interpreter.js
@@ -0,0 +1,64 @@
+// var hello = "interesting";
+
+// function example(){
+// 	var hello = 'hi!'
+// 	console.log(hello)
+// }
+
+// example();
+// console.log(hello);
+
+//output prediction
+	//hi!
+	//interesting
+
+//-----------------------------------------
+
+//Problem 1
+// var first_variable = "Yipee I was first!";
+// function firstFunc() {
+//   first_variable = "Not anymore!!!";
+//   console.log(first_variable);
+// }
+
+// console.log(first_variable);
+// console.log(first_variable);
+
+//predicted output
+	//Yippee I was first!
+	//Yippee I was first!
+
+//-----------------------------------------
+
+//Problem 2
+// var food = "Chicken";
+
+// function eat() {
+// 	var food = "gone";  
+//   	food = "half-chicken";
+//   	console.log(food);
+//        // CAREFUL!
+//  	console.log(food);
+// }
+
+// eat();
+// console.log(food);
+
+//predicted output
+	//half-chicken
+	//half-chicken
+	//Checkin
+
+//-----------------------------------------
+
+//Problem 3
+var new_word = "NEW!";
+
+function lastFunc() {
+  new_word = "old";
+}
+
+console.log(new_word);
+
+//predicted output 
+	//NEW!
---
+++ js_fundamentals/JSFundPartI/scripts/function_builder.js
@@ -0,0 +1,58 @@
+//basic 1
+function runningLogger(){
+	console.log(`I am running`)
+}
+
+function multiplyByTen(val){
+	return val*10
+}
+
+multiplyByTen(5)
+
+function stringReturnOne(){
+	return 'stringOne'
+}
+
+function stringReturnTwo(){
+	return 'stringTwo'
+}
+
+function caller(param){
+	if(typeof param === "function"){
+		param()
+	}
+}
+
+function myDoubleConsoleLog(param_1, param_2){
+	if(typeof param_1 === "function"){
+		console.log(param_1())
+	}
+	if(typeof param_2 === "function"){
+		console.log(param_2())
+	}
+}
+
+function caller2(param){
+	console.log('starting')
+	function stateChange(newState) {
+    	setTimeout(function(){
+        	if(typeof param=== "function"){
+        		param()
+        	}
+        	
+    	}, 2000);
+}
+	console.log('ending?')
+
+	
+	return 'interesting'
+}
+
+var testFunc=(function(){
+	console.log('test_1')
+})
+var testFunc_2=(function(){
+	console.log('test_2')
+})
+caller2((myDoubleConsoleLog(function(){console.log(`param_1`)}, function(){console.log(`param_2`)})))
+//caller2(testFunc, testFunc_2)
