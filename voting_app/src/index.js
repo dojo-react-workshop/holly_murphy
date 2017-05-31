45110f148663d188976316fa5bc6c4c3bd1a549c
Holly Murphy <holly.m.murphy@hotmail.com>
2017-05-31 16:21:20 -0500

    final update to working voting app

--- voting_app/src/index.js
+++ voting_app/src/index.js
@@ -4,28 +4,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import Container from './Container'
 
-//  const rects = [
-//                     {   txt: 'React',
-//                         img: './plus-sign.png',
-//                         count: 0
-//                     },
-//                     {
-//                         txt: 'Redux',
-//                         img: './plus-sign.png',
-//                         count: 0
-//                     },
-//                     {
-//                         txt:'Node',
-//                         img: './plus-sign.png',
-//                         count: 0
-//                     },
-//                     {
-//                         txt: 'Express',
-//                         img: './plus-sign.png',
-//                         count: 0
-//                     }
-//                 ]
-                
+              
  ReactDOM.render(
         <Container  />, 
         document.getElementById('root')
