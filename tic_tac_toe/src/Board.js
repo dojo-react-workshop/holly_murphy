973e34765de2cee79d7dbcfaacb8827a12666ad6
Holly Murphy <h_murphy@Hollys-MacBook-Pro.local>
2017-06-01 19:37:23 -0500

    working version

--- tic_tac_toe/src/Board.js
+++ tic_tac_toe/src/Board.js
@@ -4,89 +4,23 @@ import Square from './Square.js'
 class Board extends Component {
   state={
     xOrO: 'X',
-    board: ['','','','','','','','',''],
-    game: 'in progress'
+    board: ['','','','','','','','','']
   }
 
    updateXOrO = (id) => {
-    //update your state
-    let winner='in progress'
-    
-    if(this.state.xOrO === 'X'){
-      this.setState((prevstate)=>{
-        prevstate.xOrO= 'O',
-        prevstate.board[id]='X'
-      })
-    }else{
-      this.setState((prevstate)=>{
-        prevstate.xOrO= 'X',
-        prevstate.board[id]='O'
-      })
-    }
-    winner = this.gameOver()
-    if (winner === 'X'){
-      this.setState((prevstate)=>{
-        prevstate.game='X'
-      })
-    }else if(winner ==='O'){
-      this.setState((prevstate)=>{
-        prevstate.game='O'
-      })
-    }
-  }
-
-  gameOver = () => {
-    //check all 8 conditions
-    let winnerArray = []
-    let condOne, condTwo, condThree, condFour, condFive, condSix, condSeven, condEight 
-
-    winnerArray.push(condOne = this.winner(0,1,2))
-    winnerArray.push(condTwo = this.winner(3,4,5))
-    winnerArray.push(condTwo = this.winner(6,7,8))
-    winnerArray.push(condTwo = this.winner(0,3,6))
-    winnerArray.push(condTwo = this.winner(1,4,7))
-    winnerArray.push(condTwo = this.winner(2,5,8))
-    winnerArray.push(condTwo = this.winner(0,4,8))
-    winnerArray.push(condTwo = this.winner(2,4,6))
-
-    for(let x=0; x<9; x++){
-      if(winnerArray[x] === 'X' || winnerArray[x] === 'O'){
-        return winnerArray[x]
-      }
-    }
-  
-    return 'in progress'
-    
-  }
-    winner = (iOne,iTwo,iThree) =>{
-    let xCount = 0
-    let oCount = 0
-    if(this.state.board[iOne] ==='O'){
-      oCount += 1
-    }else if(this.state.board[iOne]==='X'){
-      xCount +=1
-    }
-
-    if(this.state.board[iTwo] ==='O'){
-      oCount += 1
-    }else if(this.state.board[iTwo]==='X'){
-      xCount +=1
-    }
-
-    if(this.state.board[iThree] ==='O'){
-      oCount += 1
-    }else if(this.state.board[iThree]==='X'){
-      xCount +=1
-    }
-
-    if(xCount === 3){
-      return 'X'
-    }else if(oCount === 3){
-      return 'O'
-    }
-    return 'in progress'
-  }//end of winner
-
+    //update your state 
+        if(this.state.xOrO === 'X'){
+          this.setState((prevstate)=>{
+            prevstate.xOrO= 'O',
+            prevstate.board[id]='X'
+          })
+        }else{
+          this.setState((prevstate)=>{
+            prevstate.xOrO= 'X',
+            prevstate.board[id]='O'
+          })
+        }
+   }
  
  
   render() {
--- tic_tac_toe/src/Square.js
+++ tic_tac_toe/src/Square.js
@@ -5,15 +5,65 @@ class Square extends Component {
   render() {
 
     let letter = this.props.xOrO
-    console.log(`letter: ${letter}`)
     let arrOfBoxes = []
-
     const update = (e)=>{
-        console.log(e.target.id)
         this.props.updateXOrO(e.target.id)
     }
-    if(this.props.game === 'X' || this.props.game === 'O'){
-        arrOfBoxes.push(<div className="winner">{this.props.game} Won!</div>)
+
+    // do check if game is over
+    const checkWinner = () =>{
+        let winnerArray = []
+        let condOne, condTwo, condThree, condFour, condFive, condSix, condSeven, condEight 
+
+        winnerArray.push(condOne = winner(0,1,2))
+        winnerArray.push(condTwo = winner(3,4,5))
+        winnerArray.push(condTwo = winner(6,7,8))
+        winnerArray.push(condTwo = winner(0,3,6))
+        winnerArray.push(condTwo = winner(1,4,7))
+        winnerArray.push(condTwo = winner(2,5,8))
+        winnerArray.push(condTwo = winner(0,4,8))
+        winnerArray.push(condTwo = winner(2,4,6))
+
+        for(let x=0; x<9; x++){
+            if(winnerArray[x] === 'X' || winnerArray[x] === 'O'){
+                return  winnerArray[x]
+            }
+        }
+       return 'no winner'
+    }//checkWinner
+    
+    const winner = (iOne,iTwo,iThree) =>{
+    let xCount = 0
+    let oCount = 0
+    if(this.props.board[iOne] ==='O'){
+      oCount += 1
+    }else if(this.props.board[iOne]==='X'){
+      xCount +=1
+    }
+
+    if(this.props.board[iTwo] ==='O'){
+      oCount += 1
+    }else if(this.props.board[iTwo]==='X'){
+      xCount +=1
+    }
+
+    if(this.props.board[iThree] ==='O'){
+      oCount += 1
+    }else if(this.props.board[iThree]==='X'){
+      xCount +=1
+    }
+
+    if(xCount === 3){
+      return 'X'
+    }else if(oCount === 3){
+      return 'O'
+    }
+    return 'no winner'
+  }//end of winner
+
+
+    if(checkWinner() === 'X' || checkWinner() === 'O'){
+        arrOfBoxes.push(<div className="winner">{checkWinner()} Won!</div>)
     }else{
         arrOfBoxes.push(<div id='player'>Next player: {this.props.xOrO}</div>)
         for(let x=0; x<9; x++){
