37fd82a133beb8b32bb18bda4a35fb064b345d84
HOLLY MURPHY <MUR0580@HOLLYs-MacBook-Pro.local>
2017-06-06 08:02:42 -0500

    Updated test api

--- test_app_api/src/Display.js
+++ test_app_api/src/Display.js
@@ -5,8 +5,8 @@ class Display extends Component {
   render() {
     console.log(`in render with these props: `,this.props)
    let arr=[]
-   this.props.repos.forEach((val)=>{
-     arr.push(val.full_name)
+   this.props.repos.forEach((val, id)=>{
+     arr.push(<p key={id}>{val.full_name}</p>)
    })
    
     return (
